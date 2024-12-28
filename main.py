dc5425f413c5e10605c516bca8266516fe36ac1a
hotwopik <none>
2024-12-28 11:27:45 +0500

    database

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, session, redirect, request
+from  sqlite4  import  SQLite4
 
 HOST="localhost"
 PORT=443
+
+database=SQLite4("database.db")
 
 app = Flask(__name__)
 
@@ -11,4 +13,6 @@ def index():
     return render_template("index.html")
 
 if __name__=="__main__":
+    database.create_table("chemy_info",["name",])
+
     app.run(host=HOST,port=PORT,debug=True)
