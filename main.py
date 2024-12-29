abe2c858877fb0a5bb1d34026f88f8486cc8e01d
hotwopik <none>
2024-12-29 09:51:15 +0500

    base.html + database table

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, session, redirect, request
-from  sqlite4  import  SQLite4
+from flask import Flask, render_template, redirect, request
+from sqlite4 import SQLite4
 
 HOST="localhost"
 PORT=443
 
 database=SQLite4("database.db")
-
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+@app.route("/data")
+def data():
+    return render_template("data.html")
 
 if __name__=="__main__":
-    database.create_table("chemy_info",["name",])
+    database.connect()
+    database.create_table("chemy_info",["name","good","ph","petroleum","hloris","sulfis","bromis","ftoris","boris","natris","magnis","calcis","kalis","stroncis","plumbus","Zincum","ferrum","manganum","cuprum"])
 
     app.run(host=HOST,port=PORT,debug=True)
