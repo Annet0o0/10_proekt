78d31a28f748ca0f810fd027fde9530db8dae8a5
hotwopik <none>
2024-12-31 16:21:28 +0500

    Таблица

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ PORT=443
 CHYM=[
     ("Хлориды","Cl⁻"),
     ("Бромиды","Br⁻"),
-    ("Фториды","А⁻"),
+    ("Фториды","F⁻"),
     ("Сульфаты","SO₄²⁻"),
     
     ("Бор","B"),
     ("Натрий","Na"),
     ("Магний","Mg"),
-    ("Кальций","K"),
-    ("Калий","Ca"),
+    ("Кальций","Ca"),
+    ("Калий","K"),
     ("Стронций","Sr"),
     ("Свинец","Pl"),
     ("Цинк","Zn"),
@@ -32,34 +32,55 @@ def index():
 @app.route("/data")
 def data():
     info=database.select("info")
-    return render_template("data.html",info=info,size=len(info))
+    return render_template("data.html",info=info,info_size=len(info),chym=CHYM,chym_range=range(len(CHYM)))
 
 if __name__=="__main__":
     database.connect()
     database.create_table("info",["name","good","ph","petroleum","cl","br","f","so","b","na","mg","ca","k","sr","pl","zn","fe","mn","cu"])
 
     #Default info
-    if len(database.select("info"))==0: database.insert("info",
-    {
-        "name":"Вишера",
-        "good":10,
-        "ph":1,
-        "petroleum":0.5,
-        "cl":0.5,
-        "br":0.5,
-        "f":0.5,
-        "so":0.5,
-        "b":0.5,
-        "na":0.5,
-        "mg":0.5,
-        "ca":0.5,
-        "k":0.5,
-        "sr":0.5,
-        "pl":0.5,
-        "zn":0.5,
-        "fe":0.5,
-        "mn":0.5,
-        "cu":0.5
-    })
+    if len(database.select("info"))==0: 
+        database.insert("info",{
+            "name":"Вишера",
+            "good":10,
+            "ph":1,
+            "petroleum":0.5,
+            "cl":0.5,
+            "br":0.5,
+            "f":0.5,
+            "so":0.5,
+            "b":0.5,
+            "na":0.5,
+            "mg":0.5,
+            "ca":0.5,
+            "k":0.5,
+            "sr":0.5,
+            "pl":0.5,
+            "zn":0.5,
+            "fe":0.5,
+            "mn":0.5,
+            "cu":0.5
+        })
+        database.insert("info",{
+            "name":"Смолишена",
+            "good":10,
+            "ph":1,
+            "petroleum":0.5,
+            "cl":0.5,
+            "br":0.5,
+            "f":0.5,
+            "so":0.5,
+            "b":0.5,
+            "na":0.5,
+            "mg":0.5,
+            "ca":0.5,
+            "k":0.5,
+            "sr":0.5,
+            "pl":0.5,
+            "zn":0.5,
+            "fe":0.5,
+            "mn":0.5,
+            "cu":0.5
+        })
 
     app.run(host=HOST,port=PORT,debug=True)
