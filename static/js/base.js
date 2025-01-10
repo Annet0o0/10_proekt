afeffdca4a68da360e1aa958db4647189c3a693e
hotwopik <none>
2025-01-10 18:15:15 +0500

    Улучшение кнопки контактов

--- static/js/base.js
+++ static/js/base.js
@@ -1,15 +1,9 @@
-const anchors = document.querySelectorAll(".header__nav a")
+const footer=document.getElementById("footer");
+const contacts=document.getElementById("contacts");
 
-anchors.forEach(anc => {
-    anc.addEventListener("click", function (event) {
-        const id = anc.getAttribute("href");
-        if (id.charAt(0) === '#') {
-            event.preventDefault();
-            const elem = document.querySelector(id);
-            window.scroll({
-                top: elem.offsetTop,
-                behavior: 'smooth'
-            });
-        }
+contacts.addEventListener("click", function(e){
+    window.scroll({
+        top: footer.offsetTop,
+        behavior: 'smooth'
     });
 });
